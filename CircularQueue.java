47a77ad3e1ff11c42ddd80128880886ffea58f7e
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-08-26 12:51:08 -0300

    Criação da fila circular genérica

---
+++ CircularQueue.java
@@ -0,0 +1,87 @@
+public class CircularQueue<T> {
+    private int top = -1;
+    private int base = 0;
+    private T[] data;
+
+    @SuppressWarnings("unchecked")
+
+    public CircularQueue(int size) {
+        this.data = (T[]) new Object[size];
+    }
+
+    public void add(T data) {
+
+        if (this.isFull()) {
+            System.out.println("Impossível adicionar um dado, a fila já está cheia! \n");
+        } else {
+            // Código extremamente duvidoso cuidado!
+
+            if (this.top == -1) {
+                this.top = 0;
+            }
+            
+            base = (base + 1) % this.data.length;
+            this.data[base] = data;
+
+        }
+    }
+     
+    public T remove() {
+
+        if (this.isEmpty()) {
+            System.out.println("Impossível retirar um dado, a fila já está vazia! \n");
+        } else {
+
+            // Código de retirar dado aqui.
+
+        }
+        return null; // placeholder pro VScode não reclamar
+    }
+
+    public void clear() {
+        
+        @SuppressWarnings("unchecked")
+
+        T[] newData = (T[]) new Object[this.data.length];
+        this.data = newData;
+        this.top = -1;
+        this.base = 0;
+    }
+    
+    public boolean isFull() {
+
+        if (this.top == this.base && this.base == this.data.length - 1) {
+            return true;
+
+          } else if (this.top == this.top + 1) {
+            return true;
+
+          } else {
+            return false;
+
+          }
+    }
+
+    public boolean isEmpty() {
+
+        if (this.top == -1) {
+            return true;
+
+        } else {
+            return false;
+
+        }
+    }
+    
+    private int move(int pos) {
+
+        if (pos + 1 == this.data.length) {
+            return 0;
+
+        } else {
+            return pos++;
+
+        }
+    }
+
+}
