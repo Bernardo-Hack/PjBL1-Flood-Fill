754503645e71d9ee4a64ffc3de554385d36fdbef
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-08-28 09:55:35 -0300

    Reorganização de código
    Reorganização do código da pilha genérica (só pra ficar bonitinho)
    
    Co-Authored-By: EmyMatsu <132858538+EmyMatsu@users.noreply.github.com>
    Co-Authored-By: Gabriela Ribeiro <104264294+gribeiro08@users.noreply.github.com>

--- StaticStack.java
+++ StaticStack.java
@@ -1,94 +1,88 @@
-public class StaticStack<Par> {
-    private int top = -1; //armazena a posição do ultímo dado inserido
-    private Par[] data; //array genérico vazio
+public class StaticStack<T> {
+    private int top = -1; // Váriavel que armazena a posição do último dado inserido
+    private T[] data; // Variável do array genérico vazio
 
     @SuppressWarnings("unchecked") //Os avisos estavam irritando :p
 
-    //A função StaticStack cria a Pilha Estática
+    // Constructoe da classe StaticStack cria a pilha do tipo escolhido
     StaticStack(int size) {
+        this.data = (T[]) new Object[size];
 
-        this.data = (Par[]) new Object[size];
     }
     
-    //A função push adiciona dados a pilha
-    public void push(Par data) {
-        
+    // A função push adiciona dados a pilha
+    public void push(T data) {
         this.top++;
         if (this.isEmpty()) {
-
             this.data[0] = data;
             this.top = 0;
 
         } else {
             try {
-
                 this.data[this.top] = data;
 
             } catch(ArrayIndexOutOfBoundsException e) {
-
                 System.out.println("Impossível adicionar um dado, a Pilha já está cheio! \n");
+
             }
         }
     }
 
-    //A função pop remove o ultímo elemnto inserido e retorna o dado
-    public Par pop() {
-
+    // A função pop remove o último elemnto inserido e retorna o dado
+    public T pop() {
         try {
-            
-            Par value = this.data[top];
+            T value = this.data[top];
             this.data[top] = null;
             this.top--;
             return value;
 
         } catch (ArrayIndexOutOfBoundsException e) {
-
             System.out.println("Impossível retirar um dado, a Pilha já está vazia! \n");
             return null;
+
         }
     }
 
-    /*A função clear tem como objetivo limpar a pilha, essa função inicializa um novo array 
-    genérico com o mesmo tamanho, porem com valores nulos*/
+    /* A função clear tem como objetivo limpar a pilha, essa função o array data
+    um novo array genérico com o mesmo tamanho com valores nulos */
     public void clear() {
 
         @SuppressWarnings("unchecked")
 
-        Par[] newData = (Par[]) new Object[this.data.length];
+        T[] newData = (T[]) new Object[this.data.length];
 
         this.data = newData;
         this.top = -1;
     }
 
-    //A função isFull retorna se a estrura esta cheia
+    // A função isFull retorna se a pilha está cheia
     public boolean isFull() {
-
         if (top == this.data.length) {
             return true;
 
         } else {
             return false;
+
         }
     }
 
-    //A função isEmpty retorna se a estrutura esta vazia
+    // A função isEmpty retorna se a pilha está vazia
     public boolean isEmpty() {
-
         if (this.top == -1) {
             return true;
 
         } else {
             return false;
+
         }
     }
 
-    //A função printStack foi criada para printar a stack bonitinho
-    //não vai funcionar quando valor é instanciado com Par
+    /* A função printStack foi criada para printar a pilha bonitinho
+    não vai funcionar quando valor é instanciado com T */
     void printStack() {
-        
         System.out.print("\n");
-        for(int i = 0; i < this.data.length; i++) {
 
+        for(int i = 0; i < this.data.length; i++) {
             System.out.println(i + " : " + this.data[i]);
 
         }
